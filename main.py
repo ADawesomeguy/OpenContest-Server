62b0b2f0494645d9c4b745c8e8a36b2ab62d1b15
Anthony Wang <ta180m@gmail.com>
2021-06-12 16:03:22 -0500

    Finally got it to work

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import socket
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-s = socket.socket()
+class FileUploadRequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # Get the size of data
+        post_data = self.rfile.read(content_length) # Get the data itself
+        
+        print(post_data)
 
-s.bind(('127.0.0.1', 6000))
+        # self._set_response()
+        # self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
-s.listen(5)
-print("socket is listening")
+def run(server_class=HTTPServer, handler_class=FileUploadRequestHandler):
+    server_address = ('127.0.0.1', 6000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
 
-while True:
-    # Establish connection with client
-    c, addr = s.accept()
-    print('Received connection from', addr)
-    
-    # Close the connection with the client
-    c.close()
+run()
