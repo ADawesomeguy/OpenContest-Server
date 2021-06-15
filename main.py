b36fca4a7f7d92fa212f23b2a307bf91844d05f8
Anthony Wang <ta180m@gmail.com>
2021-06-14 21:19:44 -0500

    Update grader to grade TCs from folder

--- main.py
+++ main.py
@@ -5,49 +5,44 @@ class FileUploadRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # Get the size of data
         post_data = self.rfile.read(content_length).decode('ascii') # Get the data itself
-        
         print(post_data)
 
         program = post_data[post_data.find('\n\r\n'):post_data.rfind('\n\r\n')]
         print(program)
 
-
         f = open('main.cpp', 'w')
         f.write(program)
         f.close()
-
-        os.system('rm out')
         os.system('g++ main.cpp -o main -O2')
-        ret = os.system('timeout 1 ./main < in > out')
 
-        os.system('rm main.cpp main')
+        tc = 1
+        while os.path.isfile(str(tc)+'.in'):
+            os.system('rm out')
+            ret = os.system('timeout 1 ./main < 1/'+str(tc)+'.in > out')
 
-        if ret == 124:
-            print('Timed out')
-
-            self.send_response(408)
-            self.send_header('Access-Control-Allow-Origin', '*')
-            self.end_headers()
+            if ret == 124:
+                print('Timed out')
+                os.system('rm main.cpp main')
+                self.send_response(408)
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                return
             
-            return
-        
-        ret = os.system('diff -w out ans')
+            ret = os.system('diff -w out '+str(tc)+'.out')
 
-        if ret != 0:
-            print('WA')
-
-            self.send_response(406)
-            self.send_header('Access-Control-Allow-Origin', '*')
-            self.end_headers()
-            
-            return
+            if ret != 0:
+                print('WA')
+                os.system('rm main.cpp main')
+                self.send_response(406)
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                return
         
         print('AC')
-
+        os.system('rm main.cpp main')
         self.send_response(202)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-
 
 
 def run(server_class=HTTPServer, handler_class=FileUploadRequestHandler):
@@ -55,4 +50,6 @@ def run(server_class=HTTPServer, handler_class=FileUploadRequestHandler):
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
+
 run()
+
