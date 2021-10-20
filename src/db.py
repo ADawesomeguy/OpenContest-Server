45b8acfe6d72f764db8f3d080312a16d679f6915
Andy Teh <spicecat@exozy.me>
2021-10-20 12:56:13 -0500

    fixed db directory

--- src/db.py
+++ src/db.py
@@ -2,7 +2,9 @@
 
 from sqlite3 import connect
 
+from var import cwd
+
 # Prepare db
-con = connect('db', check_same_thread=False)
+con = connect(cwd+'db', check_same_thread=False)
 cur = con.cursor()
 print('database connected')
