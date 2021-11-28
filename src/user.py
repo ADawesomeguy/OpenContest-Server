be8534fd74817e65b459d143ef467b4b22022f45
Anthony Wang <ta180m@pm.me>
2021-11-27 19:42:23 -0600

    Support OpenContest v2.0 user requests

--- src/user.py
+++ src/user.py
@@ -4,53 +4,73 @@ import os
 import hashlib
 import sqlite3
 from operator import itemgetter
-# import jwt
 
 from db import con, cur
-
-
-def hash(password, salt):
-    return salt+hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
-
-
-def generate_token(username):
-    return username
-    # return jwt.encode({'sub': username}, tokenKey)
-
-
-def register(data):  # Register a new user
-    try:
-        username, password, name, email = itemgetter(
-            'username', 'password', 'name', 'email')(data)
-    except KeyError:
-        return 400
-    try:
-        cur.execute('INSERT INTO users VALUES (?, ?, ?, ?)',
-                (name, email, username, hash(password, os.urandom(32))))
-        con.commit()
-    except sqlite3.IntegrityError:
-        return 409
-    return generate_token(username), 201
-
-
-def login(data):  # TODO: Login and return token
-    token = False
-    try:
-        if authenticate(data):
-            token = generate_token(data['username'])
-            return token
-        else:
-            return 401
-    except Exception:
-        return 400
-
-
-def authenticate(data):  # Authenticate user: verify password
-    users = cur.execute(
-        'SELECT * FROM users WHERE username = ?', (data['username'],)).fetchall()
-    return len(users) == 1 and users[0][3] == hash(data['password'], users[0][3][:32])
-
 
 # Create user table
 cur.execute(
     'CREATE TABLE IF NOT EXISTS users (username text unique, name text, email text unique, password text)')
+
+# Create tokens object
+tokens = dict()
+
+# Hash password with salt
+def hash(password, salt):
+    return salt + hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+
+# Handle register requests
+# Register a new user
+def register(data):
+    try:
+        username, password, name, email = itemgetter('username', 'password', 'name', 'email')(data)
+    except KeyError:
+        return (400, None)
+    
+    if cur.execute('SELECT Count(*) FROM users WHERE username=?', (username,)).fetchone()[0] != 0:
+        return (409, None)
+    
+    cur.execute('INSERT INTO users VALUES (?, ?, ?, ?)',
+        (name, email, username, hash(password, os.urandom(32))))
+    con.commit()
+    return (201, None)
+
+# Handle authenticate requests
+# Verify username and password
+def authenticate(data):
+    try:
+        username, password = itemgetter('username', 'password')(data)
+    except KeyError:
+        return (400, None)
+
+    users = cur.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchall()
+    if len(users) == 0:
+        return (404, None) # Username not found
+    
+    if users[0][3] == hash(password, users[0][3][:32]):
+        # Create and save token
+        token = os.urandom(32).encode('base-64')
+        if username not in tokens:
+            tokens[username] = set()
+        tokens[username].add(token)
+        return (200, token)
+    
+    return (403, None) # Incorrect password
+
+# Handle authorize requests
+# Verify token
+def authorize(data):
+    try:
+        username, token = itemgetter('username', 'token')(data)
+    except KeyError:
+        return (400, None)
+    
+    if cur.execute('SELECT Count(*) FROM users WHERE username=?', (username,)).fetchone()[0] == 0:
+        return (404, None) # Username not found
+    
+    if username not in tokens:
+        tokens[username] = set()
+    if token in tokens[username]:
+        tokens[username].remove(token)
+        return (200, None)
+    
+    return (403, None) # Incorrect token
