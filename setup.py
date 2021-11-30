deb305f3b5550bb24bff986e06001b3fde06eba9
Anthony Wang <ta180m@pm.me>
2021-11-30 11:10:22 -0600

    Use lowercase name for Python package in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='OpenContest-Server',
+    name='opencontest-server',
     version='2.1.0',
     author='Anthony Wang',
     author_email='ta180m@pm.me',
