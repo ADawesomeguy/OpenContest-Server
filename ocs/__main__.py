9692a191552ecb445c5d7827ceeba8e337e38afb
Anthony Wang <ta180m@pm.me>
2022-01-15 20:03:22 -0600

    Add requests as Python dependency in setup.cfg

--- ocs/__main__.py
+++ ocs/__main__.py
@@ -8,6 +8,19 @@ from ocs.args import args
 from ocs.server import server
 
 
+# Verbose logging
+if args.verbose:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
+
+# Make sure directories exist
+if not os.path.isdir(args.data_dir):
+    os.makedirs(args.data_dir, exist_ok=True)
+if not os.path.isdir(args.contests_dir):
+    os.makedirs(args.contests_dir, exist_ok=True)
+
+
 def main():
     """Run the server"""
     httpd = ThreadingHTTPServer(('localhost', args.port), server)
--- ocs/args.py
+++ ocs/args.py
@@ -15,15 +15,3 @@ parser.add_argument('-d', '--data-dir', default=os.path.join(os.getcwd(), 'data'
 parser.add_argument('-c', '--contests-dir', default=os.path.join(os.getcwd(), 'contests'),
                     help='Contests directory', type=str)
 args = parser.parse_args()
-
-# Verbose logging
-if args.verbose:
-    logging.basicConfig(level=logging.DEBUG)
-else:
-    logging.basicConfig(level=logging.INFO)
-
-# Make sure directories exist
-if not os.path.isdir(args.data_dir):
-    os.makedirs(args.data_dir, exist_ok=True)
-if not os.path.isdir(args.contests_dir):
-    os.makedirs(args.contests_dir, exist_ok=True)
