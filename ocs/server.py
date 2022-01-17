9bed683fa201ce3b62c98766acd1b8c5f5b1f3e5
Anthony Wang <ta180m@pm.me>
2022-01-17 13:55:55 -0600

    Encode JSON body in HTTP response as utf-8

--- ocs/server.py
+++ ocs/server.py
@@ -26,7 +26,7 @@ class server(BaseHTTPRequestHandler):
         if body is None:
             self.end_headers()
         else:
-            body = dumps(body)
+            body = dumps(body).encode('utf-8')
             self.send_header('Content-Type', 'application/json')
             self.send_header('Content-Length', str(len(body)))
             self.end_headers()
